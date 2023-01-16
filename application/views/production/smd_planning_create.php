6a9a4612871b8918318440bf6733819a96e9f89d
Amalia Safira <amaliayuya@gmail.com>
2023-01-16 17:08:17 +0700

    update

--- application/views/production/smd_planning_create.php
+++ application/views/production/smd_planning_create.php
@@ -114,6 +114,114 @@
     .container__months {
         width: 280px !important;
     }
+
+    .top-25 {
+        top: 25% !important;
+    }
+
+    .start-25 {
+        left: 25% !important;
+    }
+
+    .start-10 {
+        left: 10% !important;
+    }
+
+    .w-35 {
+        width: 35% !important;
+    }
+
+    .bd-callout-0 {
+        border-left-color: #9b5fe0;
+        border-left-width: 10px;
+    }
+
+    .bd-callout-1 {
+        border-left-color: #16a4d8;
+        border-left-width: 10px;
+    }
+
+    .bd-callout-2 {
+        border-left-color: #60dbe8;
+        border-left-width: 10px;
+    }
+
+    .bd-callout-3 {
+        border-left-color: #8bd346;
+        border-left-width: 10px;
+    }
+
+    .bd-callout-4 {
+        border-left-color: #efdf48;
+        border-left-width: 10px;
+    }
+
+    .bd-callout-5 {
+        border-left-color: #f9a52c;
+        border-left-width: 10px;
+    }
+
+    .bd-callout-6 {
+        border-left-color: #d64e12;
+        border-left-width: 10px;
+    }
+
+    .bg-callout-0 {
+        background-color: #9b5fe0;
+        opacity: 0.5;
+    }
+
+    .bg-callout-1 {
+        background-color: #16a4d8;
+        opacity: 0.5;
+    }
+
+    .bg-callout-2 {
+        background-color: #60dbe8;
+        opacity: 0.5;
+    }
+
+    .bg-callout-3 {
+        background-color: #8bd346;
+        opacity: 0.5;
+    }
+
+    .bg-callout-4 {
+        background-color: #efdf48;
+        opacity: 0.5;
+    }
+
+    .bg-callout-5 {
+        background-color: #f9a52c;
+        opacity: 0.5;
+    }
+
+    .bg-callout-6 {
+        background-color: #d64e12;
+        opacity: 0.5;
+    }
+
+    .table {
+        border-color: #808080 !important;
+    }
+
+    .bg-dark-grey {
+        background-color: #808080;
+        color: while;
+    }
+
+    .text-black {
+        color: black;
+    }
+
+
+    .card-daily {
+        transition: transform 0.2s ease;
+    }
+
+    .card-daily :hover {
+        transform: scale(1.1);
+    }
 </style>
 <main>
     <!-- Main page content-->
@@ -152,32 +260,128 @@
                                             <div class="col-12">
                                                 <nav>
                                                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                                        <button style="width: 100px;" class="p-3 nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">SKM</button>
-                                                        <button style="width: 100px;" class="p-3 nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">SKT</button>
+                                                        <button style="width: 100px;" class="p-3 nav-link position-relative active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">SKM
+                                                            <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+                                                            </span>
+                                                        </button>
+                                                        <button style="width: 100px;" class="p-3 nav-link position-relative" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">SKT
+                                                            <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+                                                            </span>
+                                                        </button>
                                                     </div>
                                                 </nav>
                                                 <div class="tab-content" id="nav-tabContent">
                                                     <div class="row">
                                                         <div class="col-12">
-                                                            <div class="card shadow-sm mb-4 small">
-                                                                <div class="card-header">
-                                                                    Main Planning
-                                                                </div>
+                                                            <div class="card shadow-none mb-4 mt-4 small position-relative">
+                                                                <h3><span class="position-absolute top-0 start-10 translate-middle badge  bg-primary">Main Planning </span></h3>
                                                                 <div class="card-body">
                                                                     <div class="row">
-                                                                        <div class="col-12 col-md-4">
-                                                                            <p class="m-0" style="font-size: 12px;"><b>Date Range</b></p>
-                                                                            <input type="text" class="form-control form-control-sm litepicker" required="required" id="dateRange">
+                                                                        <div class="col-12 col-md-6">
+                                                                            <p class="m-0 mb-2" style="font-size: 12px;"><b>Date Range</b></p>
+                                                                            <input type="text" class="form-control form-control-sm litepicker mb-2" required="required" id="dateRange">
+                                                                            <p class="m-0 mb-2" style="font-size: 12px;"><b>Goal Produksi</b></p>
+                                                                            <div class="row mb-2">
+                                                                                <div class="col-4">
+                                                                                    <select name="" id="input" class="form-control form-control-sm" required="required">
+                                                                                        <option value=""></option>
+                                                                                    </select>
+                                                                                </div>
+                                                                                <div class="col-3">
+                                                                                    <input type="text" name="" id="input" class="form-control form-control-sm" value="">
+                                                                                </div>
+                                                                                <div class="col-3 align-self-center">
+                                                                                    Box
+                                                                                </div>
+                                                                                <div class="col-2 text-end">
+                                                                                    <button type="button" class="btn btn-success btn-sm"><i class="fa fa-plus"></i></button>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="col-12 col-md-6">
+                                                                            <p class="m-0 mb-2" style="font-size: 12px;"><b>Preview</b></p>
+                                                                            <div class="card shadow-none">
+                                                                                <div class="card-body h-100">
+                                                                                    <!-- <div class="row d-flex align-items-center h-100">
+                                                                                        <div class="col text-center">
+                                                                                            <i class="small">Isi Terlebih Dahulu <b>Main Planning</b> agar dapat membuka Preview</i>
+                                                                                        </div>
+                                                                                    </div> -->
+                                                                                    <div class="card shadow-sm card-daily">
+                                                                                        <div class="card-body">
+
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="card shadow-sm mb-4 small">
-                                                                <div class="card-header">
-                                                                    Daily Planning
-                                                                </div>
+                                                            <div class="card shadow-none mb-4 small position-relative">
+                                                                <h3><span class="position-absolute top-0 start-10 translate-middle badge  bg-primary">Daily Planning </span></h3>
                                                                 <div class="card-body">
+                                                                    <div id="dataDailyPlanning">
+                                                                        <!-- <div class="row d-flex align-items-center" style="height: 200px;">
+                                                                        <div class="col text-center p-5">
+                                                                            <i class="small">Isi Terlebih Dahulu <b>Main Planning</b> agar dapat mengisi Daily Planning</i>
+                                                                        </div>
+                                                                    </div> -->
+                                                                        <div>
+                                                                            <h5 class="mt-3" id="conveying-meaning-to-assistive-technologies">Generate Otomatis Rencana Produksi dari Goal Produksi</h5>
+                                                                            <p class="mb-3">Goal Produksi yang tertera diatas akan di generate otomatis harian dibawah ini. Jika terdapat perubahan pada setiap mesin atau jumlah rencana, maka anda dapat mengubahnya sendiri. Anda dapat monitoring setiap goal pada preview diatas.</p>
+                                                                        </div>
+                                                                        <?php $a = 0;
+                                                                        for ($j = 0; $j < 14; $j++) { ?>
+                                                                            <div class="card shadow-sm bd-callout-<?= $a ?> mb-5 ">
+                                                                                <div class="card-body">
+                                                                                    <p class="m-0 mb-2"><b>Tanggal : 16 Januari 2023</b></p>
 
+                                                                                    <table class="table table-bordered table-hover table-sm">
+                                                                                        <thead>
+                                                                                            <tr class="bg-grey text-dark">
+                                                                                                <th class="p-2">Mesin</th>
+                                                                                                <th class="p-2">Satuan</th>
+                                                                                                <?php for ($i = 0; $i < 10; $i++) { ?>
+                                                                                                    <th class="p-2">ABF 12</th>
+                                                                                                <?php } ?>
+                                                                                            </tr>
+                                                                                        </thead>
+                                                                                        <tbody>
+                                                                                            <tr class="bg-callout-<?= $a ?> text-black">
+                                                                                                <td colspan="12" class="p-2"><b>MAKER</b></td>
+                                                                                            </tr>
+                                                                                            <?php for ($k = 0; $k < 3; $k++) { ?>
+                                                                                                <tr>
+                                                                                                    <td>MK-A</td>
+                                                                                                    <td>Tray</td>
+                                                                                                    <?php for ($i = 0; $i < 10; $i++) { ?>
+                                                                                                        <td></td>
+                                                                                                    <?php } ?>
+                                                                                                </tr>
+                                                                                            <?php } ?>
+                                                                                            <tr class="bg-callout-<?= $a ?> text-black">
+                                                                                                <td colspan="12" class="p-2"><b>HLP</b></td>
+                                                                                            </tr>
+                                                                                            <?php for ($k = 0; $k < 3; $k++) { ?>
+                                                                                                <tr>
+                                                                                                    <td>MK-A</td>
+                                                                                                    <td>Tray</td>
+                                                                                                    <?php for ($i = 0; $i < 10; $i++) { ?>
+                                                                                                        <td></td>
+                                                                                                    <?php } ?>
+                                                                                                </tr>
+                                                                                            <?php } ?>
+                                                                                        </tbody>
+                                                                                    </table>
+                                                                                </div>
+                                                                            </div>
+                                                                        <?php $a++;
+                                                                            if ($a == 7) {
+                                                                                $a = 0;
+                                                                            }
+                                                                        } ?>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
