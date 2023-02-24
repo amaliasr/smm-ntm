bc2e1d5659c2e1abc822bca355cb994866a5fcd6
Amalia Safira <amaliayuya@gmail.com>
2023-02-24 17:11:54 +0700

    update

--- application/views/production/warehouse_management.php
+++ application/views/production/warehouse_management.php
@@ -544,18 +544,22 @@
                                     </div>
                                     <div class="col">
                                         <div class="float-end">
-                                            <button type="button" class="btn btn-outline-dark btn-sm me-1" onclick=""><span class="fa fa-refresh"></span></button>
+                                            <button type="button" class="btn btn-outline-dark btn-sm" onclick=""><span class="fa fa-refresh"></span></button>
                                             <button class="btn btn-outline-dark btn-sm dropdown-toggle" id="dropdownMenuButton2" type="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
-                                                Option
+                                                Filter Option <span class="text-grey ms-1 me-1">( Detail, Table )</span>
                                             </button>
-                                            <button type="button" class="btn btn-primary btn-sm me-1" onclick=""><span class="fa fa-plus me-2"></span>Add Transaction</button>
+                                            <button type="button" class="btn btn-primary btn-sm" onclick=""><span class="fa fa-plus me-2"></span>Add Transaction</button>
                                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
                                                 <li>
-                                                    <h6 class="dropdown-header">Utilities</h6>
-                                                </li>
-
-                                                <li>
                                                     <h6 class="dropdown-header">Show By</h6>
+                                                </li>
+                                                <li><a class="dropdown-item">Detail <i class="ms-2 fa fa-check text-success" id="successAll"></i></a></li>
+                                                <li><a class="dropdown-item">Group <i class="ms-2 fa fa-check text-success d-none" id="successSkm"></i></a></li>
+                                                <li>
+                                                    <hr>
+                                                </li>
+                                                <li>
+                                                    <h6 class="dropdown-header">Templates</h6>
                                                 </li>
                                                 <li><a class="dropdown-item">Table <i class="ms-2 fa fa-check text-success" id="successAll"></i></a></li>
                                                 <li><a class="dropdown-item">Cardboard <i class="ms-2 fa fa-check text-success d-none" id="successSkm"></i></a></li>
@@ -1262,34 +1266,6 @@
             data: [12, 3, 2, 1, 8, 8, 2, 2, 3, 5, 7, 1]
         }]
     };
-
-    // var ctx = document.getElementById("LineWithLine").getContext("2d");
-
-    // Chart.types.Line.extend({
-    //     name: "LineWithLine",
-    //     draw: function() {
-    //         Chart.types.Line.prototype.draw.apply(this, arguments);
-
-    //         var point = this.datasets[0].points[this.options.lineAtIndex]
-    //         var scale = this.scale
-
-    //         // draw line
-    //         this.chart.ctx.beginPath();
-    //         this.chart.ctx.moveTo(point.x, scale.startPoint + 24);
-    //         this.chart.ctx.strokeStyle = '#ff0000';
-    //         this.chart.ctx.lineTo(point.x, scale.endPoint);
-    //         this.chart.ctx.stroke();
-
-    //         // write TODAY
-    //         this.chart.ctx.textAlign = 'center';
-    //         this.chart.ctx.fillText("TODAY", point.x, scale.startPoint + 12);
-    //     }
-    // });
-
-    // new Chart(ctx).LineWithLine(data, {
-    //     datasetFill: false,
-    //     lineAtIndex: 2
-    // });
 
     function openNewMaterialRequest() {
         var url = '<?= base_url() ?>production/createMaterialRequest'
